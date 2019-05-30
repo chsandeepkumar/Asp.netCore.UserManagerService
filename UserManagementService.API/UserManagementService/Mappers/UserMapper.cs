c6f65363a29287d7da3bc63055fcfe1c0e0c4640
chsandeepkumar <ch.sandeepreddy544@gmail.com>
2019-05-29 22:58:32 -0500

    [CHSk2] Renamed the Folder Name UserManagementService to UserManagementServiceAPI

---
+++ UserManagementService.API/UserManagementService/Mappers/UserMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using UserManagementService.Entities;
+using UserManagementService.Models;
+
+namespace UserManagementService.Mappers
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<ApplicationUserViewModel, IdentityUser>().ReverseMap();
+            CreateMap<ApplicationUserViewModel, ApplicationUser>()
+                .IncludeBase<ApplicationUserViewModel, IdentityUser>().ReverseMap();
+        }
+     
+    }
+}
